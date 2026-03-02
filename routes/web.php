bcd314d19d2fd98fcc1855db61359392fb7ef024
DnBright <dnbright000@gmail.com>
2026-03-03 01:30:09 +0700

    Fix: resolve client-side exceptions and update Git config

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('beranda');
 });
+
+Route::get('/live-class', function () {
+    return view('paket.live_class');
+});
+
+Route::get('/learning-package', function () {
+    return view('paket.learning_package');
+});
+
+Route::get('/certification-test', function () {
+    return view('paket.certification_test');
+});
+
+Route::get('/one-on-one', function () {
+    return view('paket.one_on_one');
+});
+
+Route::get('/pronunciation', function () {
+    return view('paket.pronunciation');
+});
