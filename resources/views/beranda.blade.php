bfa83396a5bff71351d28aea8d661e85f1527534
DnBright <dnbright000@gmail.com>
2026-03-03 01:33:09 +0700

    Fix: resolve client-side exceptions and update Git config

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -387,7 +387,7 @@
 				</div>
 												<div class="col-12">
 					<div class="gap-10 dklsaj w-100" style="">
-												<a href="https://englishvit.com/program/category/1?origin=/">
+												<a href="{{ url("/live-class") }}">
 							<div class="boxr-8 pd-10 m_pd-8 d-flex !m_show !m_center-text-align gap-10 ctgoo hover:bg-info-1 boxs-d">
 								<img src="{{ asset("images/live.webp") }}" alt="Live Class Englishvit">
 								<div class="text-left !m_center-text">
@@ -396,7 +396,7 @@
 								</div>
 							</div>
 						</a>
-												<a href="https://englishvit.com/program/category/4?origin=/">
+												<a href="{{ url("/one-on-one") }}">
 							<div class="boxr-8 pd-10 m_pd-8 d-flex !m_show !m_center-text-align gap-10 ctgoo hover:bg-info-1 boxs-d">
 								<img src="{{ asset("images/ooo.webp") }}" alt="One on One Englishvit">
 								<div class="text-left !m_center-text">
@@ -405,16 +405,26 @@
 								</div>
 							</div>
 						</a>
-												<a href="https://englishvit.com/program/category/3?origin=/">
+						</a>
+												<a href="{{ url("/learning-package") }}">
 							<div class="boxr-8 pd-10 m_pd-8 d-flex !m_show !m_center-text-align gap-10 ctgoo hover:bg-info-1 boxs-d">
-								<img src="{{ asset("images/lp.webp") }}" alt="Certification Test Englishvit">
+								<img src="{{ asset("images/lp.webp") }}" alt="Learning Package Englishvit">
 								<div class="text-left !m_center-text">
-									<p class="f-body1 m_f-body2 fc-black-8 fw-bold mb-0">Certification Test</p>
-									<p class="f-body2 fc-black-6 mb-0">Test sertifikasi kemampuan dengan standar yang diakui.</p>
+									<p class="f-body1 m_f-body2 fc-black-8 fw-bold mb-0">Learning Package</p>
+									<p class="f-body2 fc-black-6 mb-0">Paket belajar mandiri dengan materi terstruktur.</p>
 								</div>
 							</div>
 						</a>
-												<a href="https://englishvit.com/program/category/1?origin=/">
+												<a href="{{ url("/pronunciation") }}">
+							<div class="boxr-8 pd-10 m_pd-8 d-flex !m_show !m_center-text-align gap-10 ctgoo hover:bg-info-1 boxs-d">
+								<img src="{{ asset("images/live.webp") }}" alt="Pronunciation Englishvit">
+								<div class="text-left !m_center-text">
+									<p class="f-body1 m_f-body2 fc-black-8 fw-bold mb-0">Pronunciation</p>
+									<p class="f-body2 fc-black-6 mb-0">Perbaiki cara pengucapan kata dalam bahasa Inggris.</p>
+								</div>
+							</div>
+						</a>
+						<a href="{{ url("/learning-package") }}">
 							<div class="boxr-8 pd-10 d-flex-center m_d-flex-column-center gap-10 bg-info-1 ctgoo boxs-d" style="height: 100%;">
 								<img src="{{ asset("images/more.webp") }}" alt="more Englishvit">
 								<p class="f-body2 fw-bold mb-0 fc-info-7 !m_center-text" style="min-width: 100px;">Lainnya</p>
